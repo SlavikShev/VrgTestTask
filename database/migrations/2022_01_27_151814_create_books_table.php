209393de18b6e8574a25e8360353e8fd9a77a437
vyacheslav.shevchuk <12061998s>
2022-01-27 18:40:52 +0200

    added author and book models, controllers, migrations, routes and blades for it

---
+++ database/migrations/2022_01_27_151814_create_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('short_description')->default('');
+            $table->string('image')->default('');
+            $table->timestamp('publication_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
