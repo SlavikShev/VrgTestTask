61792f55426d676b49b624c2b64c3b75e874b7e2
vyacheslav.shevchuk <12061998s>
2022-01-28 09:09:59 +0200

    included js and jquery cdn

--- resources/views/authors/index.blade.php
+++ resources/views/authors/index.blade.php
@@ -4,4 +4,38 @@
 
 @section('content')
 
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Create author
+    </button>
+    {{--  move modal to balde and pass there form html and rename example fields  --}}
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ route('authors.store') }}" method="POST">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Name</label>
+                            <input type="text" class="form-control" id="name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="surname" class="form-label">Surname</label>
+                            <input type="text" class="form-control" id="surname">
+                        </div>
+                        <div class="mb-3">
+                            <label for="patronymic" class="form-label">Patronymic</label>
+                            <input type="text" class="form-control" id="patronymic">
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
